c8f16de9e8e3b945d59fc4e87c6b29a37ecfe9b4
vibes <unknown-user@fe80::250f:cb65:4624:68e4%14>
2012-10-26 15:15:44 +0200

    ThreadRules

--- ThreadRules.cs
+++ ThreadRules.cs
@@ -1,12 +1,29 @@
 using System;
+using System.Threading;
 
 namespace _Sorter
 {
 	public class ThreadRules
 	{
+		Tamagotchi tama;
+		
 		public ThreadRules ()
 		{
+			
+			
+			
+		}
+		
+		public Tamagotchi Tama {
+			get {
+				return tama;
+			}
+		}
+		
+		// Méthode boucle du thread
+		public void ThreadLoop()
+		{
+			
 		}
 	}
 }
-
